f16f3183ca9d6080c0e77fb5ec42a31adf0380a1
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-10-17 10:19:55 -0300

    carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -3,9 +3,9 @@
 import Image from "next/image";
 import { useState } from "react";
 import styles from "./styles.module.scss";
-import casaco from '@/assets/casaco.png'
-import casaco1 from '@/assets/casaco1.png'
-import blender from '@/assets/blender.webp'
+import casaco from "@/assets/casaco.png";
+import casaco1 from "@/assets/casaco1.png";
+import blender from "@/assets/blender.webp";
 
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
@@ -14,45 +14,66 @@ export const Carousel = () => {
   const [init, setInit] = useState(0);
   const [end, setEnd] = useState(3);
   const [open, setOpen] = useState(true);
-  const imgs = [casaco1, casaco1, casaco1, blender, blender,blender,casaco,casaco,casaco];
-  const increaseCarousel = (newInitValue:number = 3, newEndValue:number=3) => {
-    if(end   >= imgs.length ){
-             
-      setOpen(false)
+  const imgs = [
+    casaco1,
+    casaco1,
+    casaco1,
+    blender,
+    blender,
+    blender,
+    casaco,
+    casaco,
+    casaco,
+  ];
+  
+  const updateCarousel = (
+    newValueInit: number = 0,
+    newValueEnd: number = 3,
+  ) => {
+    const increment =( ) =>  {
+      setInit( (oldInit) => oldInit + newValueInit);
+      setEnd((oldEnd) => oldEnd + newValueEnd);
+    }
+    const decrement =() =>  {
+      setInit((oldInit) => oldInit - newValueInit);
+      setEnd((oldEnd) => oldEnd - newValueEnd);
+    }
+    const reset = () => {
       setInit(0)
       setEnd(3)
-      
+    }
+    return {reset, increment,decrement}
+  };
+  const increaseCarousel = () => {
+    if (end >= imgs.length) {
+      setOpen(false);
+      updateCarousel().reset();
       setTimeout(() => setOpen(true), 300);
-      return 
-  }
-  setOpen(false);
-  setInit(init + 3 );
-  setEnd(end + 3);
-  
-  setTimeout(() => setOpen(true), 300);
-  
-  }
-  const decreaseCarousel=() => {
-    if(init <= 0){
-    
-      setOpen(false)
-      setInit(imgs.length -3)
-      setEnd(imgs.length)
+      return;
+    }
+    setOpen(false);
+    updateCarousel(3, 3).increment();
+
+    setTimeout(() => setOpen(true), 300);
+  };
+  const decreaseCarousel = () => {
+    if (init <= 0) {
+      setOpen(false);
+      updateCarousel().reset();
       setTimeout(() => setOpen(true), 300);
-      return
-  }
-setOpen(false);
-setInit(init - 3);
-setEnd(end - 3);
-setTimeout(() => setOpen(true), 300);
-  }
+      return;
+    }
+    setOpen(false);
+    updateCarousel( 3, 3).decrement();
+    setTimeout(() => setOpen(true), 300);
+  };
 
   const handleIncreaseCarousel = () => {
-    increaseCarousel()
-  }
-const handleDecreaseCarousel =() => {
-  decreaseCarousel()
-}
+    increaseCarousel();
+  };
+  const handleDecreaseCarousel = () => {
+    decreaseCarousel();
+  };
 
   return (
     <section className={styles.wrapperCarousel}>
@@ -67,22 +88,13 @@ const handleDecreaseCarousel =() => {
         />
       ))}
       <div>
-      <button
-        onClick={
-            handleIncreaseCarousel
-        }
-      >
-        <IoIosArrowForward />
-      </button>
-      <button
-        onClick={() => {
-       
-          handleDecreaseCarousel
-       
-        }}
-      >
-        <IoIosArrowBack />
-      </button>
+        <button onClick={handleDecreaseCarousel}>
+          <IoIosArrowBack />
+        </button>
+        <button onClick={handleIncreaseCarousel}>
+          <IoIosArrowForward />
+        </button>
+
       </div>
     </section>
   );
