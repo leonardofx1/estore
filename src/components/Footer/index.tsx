f5d822969481415cf8cecd75376b3fd3daa05c81
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-10-17 14:35:40 -0300

    add links social media

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,25 @@
-import styles from './styles.module.scss'
-import leo from '@/assets/leo.png'
-import Image from 'next/image'
+import styles from "./styles.module.scss";
+import leo from "@/assets/leo.png";
+import Image from "next/image";
 
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 export const Footer = () => {
-
-    return (
-
-        <footer className={styles.footer}>
-        <div>
+  return (
+    <footer className={styles.footer}>
+      <div>
         <section className={styles.wrapperImgFooter}>
-                <Image src={leo} alt="foto do criador do site." />
-            
-        
-            </section>
-               
-                <FaGithub className={styles.gitHub}  />
-                <FaLinkedinIn className={styles.linkedin}  />
-        </div>
- 
+          <Image src={leo} alt="foto do criador do site." />
+        </section>
 
-        </footer>
-    )
-}
+        <a href="https://github.com/leonardofx1" target="blank">
+     
+          <FaGithub className={styles.gitHub} />
+        </a>
+        <a href="https://www.linkedin.com/in/leonardo-nunes-freitas-798ba1239/" target="blank">
+          <FaLinkedinIn className={styles.linkedin} />
+        </a>
+      </div>
+    </footer>
+  );
+};
