5eab1bf4ad31effc87f43367a4933a0c33237149
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-10-18 14:37:50 -0300

    Link to detail product page

--- src/components/CardsProducts/index..tsx
+++ src/components/CardsProducts/index..tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import styles from './styles.module.scss'
 
 import casaco from '@/assets/casaco.png'
+import Link from 'next/link'
 
 export const CardsProducts = () => {
 
 
     return (
+      <Link href='./detailProduct'>
         <article className={styles.cardBody}>
                 <div className={styles.cardImg}>
                 <Image src={casaco} alt='produtos' />
@@ -15,5 +17,6 @@ export const CardsProducts = () => {
             <p className={styles.cardTitle}> Camiseta polo</p>
             <p className={styles.cardBrand}> Lacoste</p>
         </article>
+      </Link>
     )
 }
