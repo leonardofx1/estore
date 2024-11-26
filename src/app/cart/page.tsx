7d6018976fee43601a2aeef597ec688d6ec5e01a
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-11-26 14:49:21 -0300

    modify design page cart

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,32 @@
+import { CardCart } from "./CardCart";
+import styles from "./styles.module.scss";
+
+export default function CartFavorites() {
+  return (
+    <section className={styles.containerMain}>
+      <div>
+        <div className={styles.wrapperCards}>
+          <CardCart />
+        </div>
+        <div className={styles.totalValueCart}>
+          <div className={styles.wrapperTotal}>
+            <p className={styles.title}>Total</p>
+
+            <div  >
+              <p>SubTotal</p>
+              <span>R$ 1000</span>
+            </div>
+            <div>
+            <p>Taxa de envio </p>
+            <span>R$ 10,00</span>
+          </div>
+          <div>
+            <p>Total </p>
+            <span>R$1100</span>
+          </div>
+          <button>Finalizar pedido</button></div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,47 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
 
-import Link from 'next/link';
-import styles from './styles.module.scss'
-
-import {  IoHome, IoMenuOutline } from "react-icons/io5";
+import { IoHome, IoMenuOutline } from "react-icons/io5";
 import { CiLogin } from "react-icons/ci";
 
 import { MdOutlineSearch } from "react-icons/md";
 import { MdFavoriteBorder } from "react-icons/md";
 export const Header = () => {
-
-
-
-    return (
-        <header className={styles.header}>
-            <nav>
-                <ul className={styles.wrapperLi}>
-                    <li>Sobre</li>
-                    <li>Suporte</li>
-                    <li>Catalogo</li>
-                </ul>
-            </nav>
-           <Link href='./'> <h1 className={styles.title}>
-                <span className={styles.markedLetter}>FX</span>estore
-            </h1></Link>
-            <ul className={styles.wrapperIconsMenu}>
-                <li><Link href='./'><IoHome /></Link></li>
-                <li><MdOutlineSearch /></li>
-           <Link href='/favorites' >     <li><MdFavoriteBorder /></li></Link>
-                <li><CiLogin /></li>    
-            </ul>
-            <IoMenuOutline className={styles.menu}/>
-        </header>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <Link href="./">
+        
+        <h1 className={styles.title}>
+          <span className={styles.markedLetter}>FX</span>estore
+        </h1>
+      </Link>
+      <nav>
+        <ul className={styles.wrapperLi}>
+          <li>Sobre</li>
+          <li>Suporte</li>
+          <li>Catalogo</li>
+        </ul>
+      </nav>
+      <ul className={styles.wrapperIconsMenu}>
+        <li>
+          <Link href="./">
+            <IoHome />
+          </Link>
+        </li>
+        <li>
+          <MdOutlineSearch />
+        </li>
+        <Link href="/favorites">
+          {" "}
+          <li>
+            <MdFavoriteBorder />
+          </li>
+        </Link>
+        <li>
+          <CiLogin />
+        </li>
+      </ul>
+      <IoMenuOutline className={styles.menu} />
+    </header>
+  );
+};
