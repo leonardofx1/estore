eebd15b792da7fa34038ca40e0ccde57534be72c
leonardofx1 <leonardonunesfreitas1@gmail.com>
2024-12-04 12:35:18 -0300

    validation zod

--- src/app/login/register/register.tsx
+++ src/app/login/register/register.tsx
@@ -2,20 +2,39 @@
 import { Dispatch, SetStateAction } from 'react'
 import styles from './styles.module.scss'
 
+import {FieldValues, SubmitHandler, useForm} from 'react-hook-form'
 
-
-
+import  * as  z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+ 
 
 
 export const Register = ({setLogin} : {setLogin: Dispatch<SetStateAction<boolean>>}) =>{
-    return (
+
+  const schemaRegister = z.object({
+    name: z.string().min(4, { message: ' insira um nome com pelo menos 4 caracteres.' }),
+    email: z.string().email({ message: 'insira um email válido, por exemplo, jjj@gmail.com' }),
+    password: z.string().min(5, { message: 'A senha deve ter pelo menos 5 caracteres.' })
+    
+  })
+
+  const{register,handleSubmit, formState:{errors}}= useForm({
+    resolver:zodResolver(schemaRegister)
+  })
+    const onSubmit:SubmitHandler<FieldValues> = (e) => {
+      console.log(e)
+    }
+  return (
         <div className={styles.formContainer}>
       <h3>Register</h3>
-      <input type="text"  placeholder='Nome'/>
-      <input type="text"  placeholder='E-mail'/>
-      <input type="text" placeholder='Senha'/>
+      <input type="text"  placeholder='Nome' {...register('name')}/>
+      {errors.name?.message && <span className={styles.errors}> {typeof errors.name.message === 'string' &&  errors.name.message }</span>}
+      <input type="text"  placeholder='E-mail' {...register('email')}/>
+      {errors.email?.message && <span className={styles.errors}> {typeof errors.email.message === 'string' && errors.email.message }</span>}
+      <input type="password" placeholder='Senha' {...register('password')}/>
+      {errors.password?.message && <span className={styles.errors}> {typeof errors.password.message === 'string' && errors.password.message  }</span>}
       <div className={styles.wrapperState}><span onClick={() => setLogin((state) => !state)}>Entrar aqui</span></div>
-      <button>Inscrever-se</button>
+      <button onClick={handleSubmit(onSubmit)}>Inscrever-se</button>
       </div>
     )
 }
--- src/app/login/userlLogin/login.tsx
+++ src/app/login/userlLogin/login.tsx
@@ -1,20 +1,45 @@
-
+'use client'
 import { Dispatch, SetStateAction } from 'react'
+import { useRouter } from 'next/navigation'
+
 import styles from './styles.module.scss'
 
 
+
+import {FieldValues, SubmitHandler, useForm} from 'react-hook-form'
+
+import  * as  z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+ 
 
 
 
 
 export const Login = ({setLogin}:{setLogin:Dispatch<SetStateAction<boolean>>}) =>{
+  const schemaLogin = z.object({
+    name: z.string().min(4, { message: ' insira um nome com pelo menos 4 caracteres.' }),
+    
+    password: z.string().min(5, { message: 'A senha deve ter pelo menos 5 caracteres.' })
+    
+  })
+  const{register,handleSubmit, formState:{errors}}= useForm({
+    resolver:zodResolver(schemaLogin)
+  }) 
+  const route = useRouter()
+  const onSubmit:SubmitHandler<FieldValues> = (e) => {
+    console.log(e)
+    route.push('/')
+  }
     return (
         <div className={styles.formContainer}>
       <h3>Login</h3>
-      <input type="text"  placeholder='Nome'/>
-      <input type="text" placeholder='Senha'/>
+
+      <input type="text"  placeholder='Nome' {...register('name')}/>
+      {errors.name?.message && <span className={styles.errors}> {typeof errors.name.message === 'string' &&  errors.name.message }</span>}
+      <input type="password" placeholder='Senha' {...register('password')}/>
+      {errors.password?.message && <span className={styles.errors}> {typeof errors.password.message === 'string' && errors.password.message  }</span>}
       <div className={styles.wrapperState}><span onClick={() => setLogin((state) => !state)}>Criar uma conta </span></div>
-      <button>Entrar</button>
+      <button onClick={handleSubmit(onSubmit)}>Entrar</button>
       </div>
     )
 }
